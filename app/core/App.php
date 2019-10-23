f4875d67f48e4264f5182585ec6cdab75d09918a
Kiki khoirul <rizkiya100@gmail.com>
2019-10-23 21:34:23 +0700

    Memperbaiki App.php pada metode routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,11 +3,11 @@
  class App{
 	public function __construct(){
 		$url = $this->parseURL();
-		var_dump($_GET);
+		var_dump($url);
 	}
 
 	public function parseURL(){
-		if (isset($_GET['url'])) {
+		if ( isset($_GET['url']) ) {
 			$url = rtrim($_GET['url'], '/');
 			$url = filter_var($url, FILTER_SANITIZE_URL);
 			$url = explode('/', $url);
